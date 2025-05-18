3e6a64d20bdbca07d1b83dc068a4407e99aa3053
Anali Tarcila <anali.tarcila@email.com>
2025-05-18 14:53:43 -0300

    Corrige app.py com código Flask funcional

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-<app.py content will be inserted in the next step>
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bem-vindo ao site de terreiros religiosos!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
